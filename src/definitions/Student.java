f567e6729144e50c28a7fd92e214d2bb45016ab0
Lenovo <garvit.gupta_cs19@gla.ac.in>
2020-08-30 13:09:04 +0530

    add getter and setter method for private fields in Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -11,4 +11,36 @@ public class Student {
     private long studentRollNumber;
     private int numberOfBooksIssued;
     private Book[] namesOfBooksIssued;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public long getStudentRollNumber() {
+        return studentRollNumber;
+    }
+
+    public void setStudentRollNumber(long studentRollNumber) {
+        this.studentRollNumber = studentRollNumber;
+    }
+
+    public int getNumberOfBooksIssued() {
+        return numberOfBooksIssued;
+    }
+
+    public void setNumberOfBooksIssued(int numberOfBooksIssued) {
+        this.numberOfBooksIssued = numberOfBooksIssued;
+    }
+
+    public Book[] getNamesOfBooksIssued() {
+        return namesOfBooksIssued;
+    }
+
+    public void setNamesOfBooksIssued(Book[] namesOfBooksIssued) {
+        this.namesOfBooksIssued = namesOfBooksIssued;
+    }
 }
